3dc69b17ca832ed77ce7dfd180e19d91dbf96604
pcoder59 <henryhyother@protonmail.com>
2021-07-09 17:03:14 +0530

    Implement Convert Download to MP3

--- main.py
+++ main.py
@@ -8,22 +8,30 @@ import moviepy.editor as mp
 
 import re
 
-print("Enter URL of YouTube Playlist: ")
+#print("Enter URL of YouTube Playlist: ")
 
-url = input()
+#url = input()
 
 print("Enter Location to Save Your File: ")
 
 Location = input()
 
-playlist = Playlist(url)
+#playlist = Playlist(url)
 
 u = "https://www.youtube.com/watch?v=iCSCF5oLrJ0"
 
-YouTube(u).streams.first().download(Location)
+fl = YouTube(u).streams.first().download(Location)
+
+new_file = mp.AudioFileClip(fl)
+
+flmp3 = fl.replace(fl[len(fl)-1], "3")
+
+new_file.write_audiofile(flmp3)
+
+os.remove(fl)
 
 #for u in playlist:
 #    print(u)
 
-for vid in playlist.videos:
-    print(vid)
+#for vid in playlist.videos:
+#    print(vid)
