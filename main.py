db57d89b1ad4df79a53a95b0679077d95a4e658c
pcoder59 <henryhyother@protonmail.com>
2021-07-09 16:40:51 +0530

    Input URL and print each

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ import os
 import moviepy.editor as mp
 
 import re
+
+print("Enter URL of YouTube Playlist: ")
+
+url = input()
+
+playlist = Playlist(url)
+
+for u in playlist:
+    print(u)
+
+for vid in playlist.videos:
+    print(vid)
